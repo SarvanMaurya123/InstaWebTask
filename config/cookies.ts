792c8341181e207527b66e573211fffe1e14dc11
SarvanMaurya123 <sarvanmaurya037@gmail.com>
2026-06-04 11:45:09 +0530

    manage cookes

--- config/cookies.ts
+++ config/cookies.ts
@@ -2,16 +2,16 @@ import { CookieOptions } from "express";
 
 export const accessTokenCookieOptions: CookieOptions = {
   httpOnly: true,
-  secure: false,
-  sameSite: "lax",
+  secure: true,
+  sameSite: "none",
   path: "/",
   maxAge: 15 * 60 * 1000,
 };
 
 export const refreshTokenCookieOptions: CookieOptions = {
   httpOnly: true,
-  secure: false,
-  sameSite: "lax",
+  secure: true,
+  sameSite: "none",
   path: "/",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
